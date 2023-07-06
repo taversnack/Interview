5056bff90a95e0d20fca3d6f610650026756b09c
Stephen Stewart <stephen.stewart@paxton-access.co.uk>
2023-07-06 22:32:30 +0100

    Added information to program and added extra test file

--- Smartwyre.DeveloperTest.Runner/Program.cs
+++ Smartwyre.DeveloperTest.Runner/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Smartwyre.DeveloperTest.Services;
+using System;
+using Smartwyre.DeveloperTest.Data;
+using Smartwyre.DeveloperTest.Types;
 
 namespace Smartwyre.DeveloperTest.Runner;
 
@@ -6,6 +9,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        throw new NotImplementedException();
+        RebateService rebateService = new RebateService(new RebateDataStore(), new ProductDataStore());
+
+        Console.Write("Enter rebate identifier: ");
+        var rebateIdentifier = Console.ReadLine();
+        Console.Write("Enter product identifier: ");
+        var productIdentifier = Console.ReadLine();
+        Console.Write("Enter volume: ");
+        var volume = Decimal.Parse(Console.ReadLine());
+
+        CalculateRebateResult result = rebateService.Calculate(new CalculateRebateRequest()
+            { RebateIdentifier = rebateIdentifier, ProductIdentifier = productIdentifier, Volume = volume }
+        );
+
+        Console.WriteLine($"Result: {result.Success}");
     }
 }
---
+++ Smartwyre.DeveloperTest.Tests/RebateService.Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smartwyre.DeveloperTest.Tests
+{
+    internal class RebateService
+    {
+    }
+}
